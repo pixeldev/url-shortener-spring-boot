2eac333e100d8b3a66f4a1629745c8c5b1a15f05
Angel Miranda <pixeldev1729@gmail.com>
2026-01-22 08:17:43 -0600

    refactor: clean up DTO validation annotations

--- src/main/java/io/github/pixeldev/urlshortener/application/dto/GetUrlShortenedRequest.java
+++ src/main/java/io/github/pixeldev/urlshortener/application/dto/GetUrlShortenedRequest.java
@@ -1,11 +1,11 @@
 package io.github.pixeldev.urlshortener.application.dto;
 
+import org.springframework.lang.Nullable;
+
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Positive;
-import org.springframework.lang.Nullable;
 
 public record GetUrlShortenedRequest(
     @NotBlank(message = "ID cannot be blank") @NotNull(message = "ID cannot be null") String id,
-    @Nullable @Positive(message = "User ID must be positive") Long userId) {
-}
+    @Nullable @Positive(message = "User ID must be positive") Long userId) {}
--- src/main/java/io/github/pixeldev/urlshortener/application/dto/GetUrlShortenedResponse.java
+++ src/main/java/io/github/pixeldev/urlshortener/application/dto/GetUrlShortenedResponse.java
@@ -1,7 +1,3 @@
 package io.github.pixeldev.urlshortener.application.dto;
 
-import org.hibernate.validator.constraints.URL;
-
-import jakarta.validation.constraints.NotBlank;
-
-public record GetUrlShortenedResponse(@NotBlank @URL String originalUrl) {}
+public record GetUrlShortenedResponse(String originalUrl) {}
